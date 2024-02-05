d211df14229127067a0261032bf2ac543e6aa54e
Vikram9392 <vikramvallala12@gmail.com>
2024-02-05 16:51:48 +0530

    done

--- src/app.js
+++ src/app.js
@@ -9,34 +9,51 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Write POST endpoint to get the sum of two number
-app.post((req,res)=>{
-  let num1=req.body.num1;
-  let num2=req.body.num2;
-  if(num1<-1000000|| num2<-1000000){
-    return res.json({
-      status:"error",
-      message:"Underflow"
-    })
-  }
-    if(num1>1000000|| num2>1000000){
-       return res.json({
-        status:"error",
-        message:"Overflow"
-      })
+app.post('/add',(req,res)=>{
+  let num1=parseInt(req.body.num1);
+  let num2=parseInt(req.body.num2);
+  // if(num1<-1000000|| num2<-1000000){
+  //   return res.json({
+  //     status:"error",
+  //     message:"Underflow"
+  //   })
+  // }
+  //   if(num1>1000000|| num2>1000000){
+  //      return res.json({
+  //       status:"error",
+  //       message:"Overflow"
+  //     })
 
-  }
- return res.json({
-    "result":req.body.num1+req.body.num2
-  })
+  // }
+//  return res.json({
+//     "result":req.body.num1+req.body.num2
+//   })
+res.send({result:num1+num2})
 })
 
 //Write POST endpoint to get the differance of two number
-
+app.post('/subtract',(req,res)=>{
+  let num1=parseInt(req.body.num1);
+  let num2=parseInt(req.body.num2);
+  res.send({result:num1-num2})
+})
 
 //Write POST endpoint to get the multiplication of two number
-
+app.post('/multiply',(req,res)=>{
+  let num1=parseInt(req.body.num1);
+  let num2=parseInt(req.body.num2);
+  res.send({result:num1*num2})
+})
 
 //Write POST endpoint to check if the num2 is 0 or not and to get the result after dividing two number
+app.post('/divide',(req,res)=>{
+  let num1=parseInt(req.body.num1);
+  let num2=parseInt(req.body.num2);
+  if(num2===0){
+    return res.send({status:'error',message:'Cannot divide by zero'})
+  }
+  res.send({result:num1/num2})
+})
 
       
 
